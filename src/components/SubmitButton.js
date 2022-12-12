05ecebf2821ab76835c588e6157e37899d46bd68
Amit Kumar <amitanwar007@gmail.com>
2022-12-12 15:23:04 +0530

    message 1

---
+++ src/components/SubmitButton.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SubmitButton() {
+    return (
+        <>
+        <div className='btnDiv'>
+        <button className='btn'>Ask</button>     
+        </div>
+        </>
+    )
+}
+
+export default SubmitButton;
